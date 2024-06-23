bf309d90ee4a2d9c51af436899103d719cf04112
Ben-Long50 <benjlong50@gmail.com>
2024-06-22 19:21:46 -0700

    setup env variables

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -2,9 +2,9 @@ import { v2 as cloudinary } from 'cloudinary';
 
 // Return "https" URLs by setting secure: true
 cloudinary.config({
-  cloud_name: 'dm4tmla72',
-  api_key: '158547294753538',
-  api_secret: 'n6NdX8ShPogZ0_z5KyS4dfCSeq8',
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
   secure: true,
 });
 
